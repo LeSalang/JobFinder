a9ac46676dc6d7f65faa58ca15bcc63ad66d9fe7
LeSa <le.salang@yandex.ru>
2024-08-23 01:56:14 +0400

    Add ViewBindingPropertyDelegate

--- screens/login/login_ui/build.gradle.kts
+++ screens/login/login_ui/build.gradle.kts
@@ -6,11 +6,9 @@ plugins {
 android {
     namespace = "com.lesa.login_ui"
     compileSdk = libs.versions.sdk.compile.get().toInt()
-
     defaultConfig {
         minSdk = libs.versions.sdk.min.get().toInt()
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -23,12 +21,18 @@ android {
     kotlinOptions {
         jvmTarget = libs.versions.jvm.target.get()
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
     // core:
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+
+    // other:
+    implementation(libs.kirich1409.viewbindingpropertydelegate)
 
     // modules:
     implementation(project(":core:ui_kit"))
