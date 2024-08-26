ae45c0a39d6454bef05acb2f8795c9f9af4afef1
LeSa <le.salang@yandex.ru>
2024-08-26 18:50:19 +0400

    Add data module

---
+++ data/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+}
+
+android {
+    namespace = "com.lesa.data"
+    compileSdk = libs.versions.sdk.compile.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.sdk.min.get().toInt()
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvm.target.get()
+    }
+}
+
+dependencies {
+    // di:
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
+
