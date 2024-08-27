fa149483d24259cb4daf3a206cb261a9fbc6a833
LeSa <le.salang@yandex.ru>
2024-08-28 01:31:22 +0400

    Add vacancy screen

---
+++ screens/vacancy/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+}
+
+android {
+    namespace = "com.lesa.vacancy"
+    compileSdk = libs.versions.sdk.compile.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.sdk.min.get().toInt()
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvm.target.get()
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    // di:
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // modules:
+    implementation(project(":core:navigation"))
+    implementation(project(":core:ui_kit"))
+    implementation(project(":data"))
+}
+
